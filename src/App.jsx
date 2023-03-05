7f6d70f19e63abb65a8594720ab554881ca3209a
Mahmoud sabry <76252661+Mahmoudshoaib@users.noreply.github.com>
2023-03-05 20:29:17 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,64 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import axios from "axios";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const [img, setImg] = useState(null);
-  useEffect(() => {
-    console.log(img);
-  });
+  const [result, setResult] = useState(null);
+  const navigate = useNavigate();
+
   return (
     <div className="App">
-      <h1 className="mb-10">
-        Identify your plant and know about diseases in it
-      </h1>
-      {img && (
-        <div>
-          <img
-            className="mx-auto"
-            src={URL.createObjectURL(img)}
-            alt="not fount"
-          />
-        </div>
+      {result ? (
+        <>
+          <h1 className="mb-10">
+            Identify your plant and know about diseases in it
+          </h1>
+          {img && (
+            <div>
+              <img
+                className="mx-auto"
+                src={URL.createObjectURL(img)}
+                alt="not fount"
+              />
+            </div>
+          )}
+          <form
+            encType="multipart/form-data"
+            className="card flex flex-col gap-14 items-center"
+            onSubmit={(e) => {
+              e.preventDefault();
+              const formData = new FormData();
+              formData.append(
+                "image",
+                document.getElementById("image").files[0]
+              );
+              fetch("http://localhost:3001/modelv1", {
+                method: "POST",
+                body: formData,
+              })
+                .then((res) => res.json())
+                .then((data) => setResult(data))
+                .catch((e) => console.log(e));
+              navigate({ pathname: "/result" });
+            }}
+          >
+            <input
+              className="bg-slate-700"
+              type="file"
+              name="image"
+              id="image"
+              accept=".jpeg, .jpg"
+              onChange={(e) => {
+                setImg(e.target.files[0]);
+              }}
+            />
+            <button type="submit">upload image</button>
+          </form>
+        </>
+      ) : (
+        console.log(result)
       )}
-      <form
-        method="GET"
-        className="card flex flex-col gap-14 items-center"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData();
-          formData.append(
-            "image",
-            document.getElementById("addImage").files[0]
-          );
-          axios
-            .post("http://localhost:3000/images", { formData })
-            .then((res) => console.log(res.data))
-            .catch((e) => console.log(e));
-          Navigate("/result");
-        }}
-      >
-        <input
-          className="bg-slate-700"
-          type="file"
-          name="addImage"
-          id="addImage"
-          accept=".jpeg, .jpg"
-          onChange={(e) => {
-            setImg(e.target.files[0]);
-          }}
-        />
-        <button type="submit">upload image</button>
-      </form>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import Results from "./Results";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/result" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
